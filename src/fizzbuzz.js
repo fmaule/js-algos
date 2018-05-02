e8a94f8f5a07cad0aeaa3de303629e6eef428509
Fernando A. Maule <fernando.maule@tesglobal.com>
2018-05-03 01:36:22 +0200

    Add eslint airbnb, fix

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,49 +1,50 @@
+/* eslint-disable no-cond-assign */
+
 // Using a for loop
 const fizzBuzzLoop = (num) => {
   const fizzes = [];
-  for (let i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i += 1) {
     if (i % 15 === 0) fizzes.push('FizzBuzz');
     else if (i % 3 === 0) fizzes.push('Fizz');
     else if (i % 5 === 0) fizzes.push('Buzz');
     else fizzes.push(i);
   }
   return fizzes;
-}
+};
 
 // Using reducer
 const fizzBuzzReducer = (num) => [...Array(num).keys()]
   .reduce((result, cv) => {
-    ++cv;
-    if (cv % 15 === 0) result.push('FizzBuzz');
-    else if (cv % 3 === 0) result.push('Fizz');
-    else if (cv % 5 === 0) result.push('Buzz');
-    else result.push(cv);
+    const currentValue = cv + 1;
+    if (currentValue % 15 === 0) result.push('FizzBuzz');
+    else if (currentValue % 3 === 0) result.push('Fizz');
+    else if (currentValue % 5 === 0) result.push('Buzz');
+    else result.push(currentValue);
     return result;
-}, []);
+  }, []);
 
 // Using map
-const fizzBuzzMap = (num) => {
-  return [...Array(num).keys()]
-    .map((x)   => ((++x % 3 === 0) ? fz = 'Fizz' : x))
-    .map((x,i) => ((++i % 5 === 0) ? bz = 'Buzz' : x))
-    .map((x,i) => ((++i % 15 === 0) ? fz + bz : x))
-}
+// shouldn't return assignment :\
+const fizzBuzzMap = (num) => [...Array(num).keys()]
+  .map((x) => ((x += 1 % 3 === 0) ? fz = 'Fizz' : x)) // eslint-disable-line no-return-assign, no-undef, no-param-reassign
+  .map((x, i) => ((i += 1 % 5 === 0) ? bz = 'Buzz' : x)) // eslint-disable-line no-return-assign, no-undef, no-param-reassign
+  .map((x, i) => ((i += 1 % 15 === 0) ? fz + bz : x)); // eslint-disable-line no-return-assign, no-undef, no-param-reassign
 
 // Using a generator function
-function * fizzBuzzGenerator() {
+function* fizzBuzzGenerator() {
   let i = 1;
   do {
-      if (i % 15 === 0) yield 'FizzBuzz';
-      else if (i % 3 === 0) yield 'Fizz';
-      else if (i % 5 === 0) yield 'Buzz';
-      else yield i;
-  } while (++i)
+    if (i % 15 === 0) yield 'FizzBuzz';
+    else if (i % 3 === 0) yield 'Fizz';
+    else if (i % 5 === 0) yield 'Buzz';
+    else yield i;
+  } while (i += 1);
 }
 
 const fizzBuzzGenerated = (num) => {
   const fizzes = [];
   const fizzBuzzer = fizzBuzzGenerator();
-  for (let i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i += 1) {
     fizzes.push(fizzBuzzer.next().value);
   }
   return fizzes;
@@ -54,4 +55,4 @@ module.exports = {
   fizzBuzzReducer,
   fizzBuzzMap,
   fizzBuzzGenerated,
-}
+};
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -4,45 +4,42 @@ const {
   fizzBuzzReducer,
   fizzBuzzMap,
   fizzBuzzGenerated,
- } = require('../src/fizzbuzz.js');
+} = require('../src/fizzbuzz.js');
 
- context('Algos', () => {
-  describe('FizzBuzz', () => {
+describe('FizzBuzz', () => {
+  const fizzedExpected = [
+    1,
+    2,
+    'Fizz',
+    4,
+    'Buzz',
+    'Fizz',
+    7,
+    8,
+    'Fizz',
+    'Buzz',
+    11,
+    'Fizz',
+    13,
+    14,
+    'FizzBuzz',
+    16,
+    17,
+    'Fizz',
+    19,
+    'Buzz',
+  ];
 
-    const fizzedExpected = [
-      1,
-      2,
-      'Fizz',
-      4,
-      'Buzz',
-      'Fizz',
-      7,
-      8,
-      'Fizz',
-      'Buzz',
-      11,
-      'Fizz',
-      13,
-      14,
-      'FizzBuzz',
-      16,
-      17,
-      'Fizz',
-      19,
-      'Buzz',
-    ]
-
-    it('implementation #1 (for loop)', () => {
-      expect(fizzBuzzLoop(20)).to.eql(fizzedExpected);
-    });
-    it('implementation #2 (reduce)', () => {
-      expect(fizzBuzzReducer(20)).to.eql(fizzedExpected);
-    });
-    it('implementation #3 (map)', () => {
-      expect(fizzBuzzMap(20)).to.eql(fizzedExpected);
-    });
-    it('implementation #4 (generator fn)', () => {
-      expect(fizzBuzzGenerated(20)).to.eql(fizzedExpected);
-    });
+  it('implementation #1 (for loop)', () => {
+    expect(fizzBuzzLoop(20)).to.eql(fizzedExpected);
+  });
+  it('implementation #2 (reduce)', () => {
+    expect(fizzBuzzReducer(20)).to.eql(fizzedExpected);
+  });
+  it('implementation #3 (map)', () => {
+    expect(fizzBuzzMap(20)).to.eql(fizzedExpected);
+  });
+  it('implementation #4 (generator fn)', () => {
+    expect(fizzBuzzGenerated(20)).to.eql(fizzedExpected);
   });
 });
